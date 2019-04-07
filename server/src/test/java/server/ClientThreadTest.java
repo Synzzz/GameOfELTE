5315cfee014ea0f63a8f8f0168059904861b6eeb
bencekiss0218 <47850146+bencekiss0218@users.noreply.github.com>
2019-04-07 17:21:24 +0200

    Tests for ClientThreadTest

--- server/src/test/java/server/ClientThreadTest.java
+++ server/src/test/java/server/ClientThreadTest.java
@@ -5,10 +5,14 @@ import com.gameofelte.util.Configuration;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,23 +21,21 @@ import static org.mockito.Mockito.spy;
 
 public class ClientThreadTest {
     private ClientThread thread;
-    private GameServer server;
-    private Socket socket;
+    private Socket socket, clientSocket;
     private PrintWriter pw;
     private Scanner sc;
-    private DummyClient dummy;
-    /*        
+            
     @BeforeEach
     public void beforeEach() throws Exception
     {
-        server = spy(new GameServer(spy(new Configuration("server.config")),6000));
-        server.start();
-        socket = new Socket("localhost", 12345);
-        pw = new PrintWriter(socket.getOutputStream(),true);
-        sc = new Scanner(socket.getInputStream());
-        
-        thread = new ClientThread(socket,server);
-        thread.start();
+        try(ServerSocket ss = new ServerSocket(12345))
+        {
+            socket = new Socket("localhost", 12345);
+            clientSocket = ss.accept();
+            sc = new Scanner(clientSocket.getInputStream());
+
+            thread = new ClientThread(socket, null);
+        }
         
         //dummy = new DummyClient();
         //dummy.start();
@@ -43,43 +45,114 @@ public class ClientThreadTest {
     @AfterEach
     public void afterEach() throws Exception
     {
-        pw.close();
-        sc.close();
+        clientSocket.close();
         socket.close();
         thread.join();
-        server.join();
         //dummy.join();
-    }
-        
-    // TODO  , lehet timeoutot kell rakni rajuk a while true-k miatt assert utan?
-    @Test
-    public void threadRunTest()
-    {
-        
     }
     
     @Test
     public void randomNumberTest()
     {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        
+        thread.sendRandomNumbers(list);
+        
+        assertEquals(sc.nextLine(), "RANDOM_NUMBERS;1,2,3");
+    }
+    
+    @Test
+    public void setMoneyTest()
+    {
+        int amountTest = 1000;
+        thread.setMoney(amountTest);
+        
+        assertEquals(sc.nextLine(), "SET_MONEY;" + amountTest);
+    }
+    
+    @Test
+    public void beginLearnSubjectTest() {
+        thread.beginLearnSubject();
+        
+        assertEquals(sc.nextLine(), "LEARN_SUBJECT");
         
     }
     
     @Test
-    public void luckyCardTest()
-    {
+    public void beginWorkOrStudyChoiceTest() {
+        thread.beginWorkOrStudyChoice();
         
+        assertEquals(sc.nextLine(),"WORK_OR_STUDY");
     }
     
     @Test
-    public void sendBoardTest()
-    {
-        
+    public void beginCourseRequestTest() {
+       thread.beginCourseRequest();
+       
+       assertEquals(sc.nextLine(),"COURSE_REQUEST");
     }
     
     @Test
-    public void sendGameOverTest()
+    public void beginLosingKnowledgeTest() {
+        thread.beginLosingKnowledge();
+        
+        assertEquals(sc.nextLine(),"LOSING_KNOWLEDGE");
+    }
+
+    @Test
+    public void beginOfferedMarkTest() {
+       thread.beginOfferedMark();
+       
+       assertEquals(sc.nextLine(),"OFFERED_MARK");
+    }
+
+    @Test
+    public void beginMatekingChoiceTest() {
+       thread.beginMatekingChoice();
+       
+       assertEquals(sc.nextLine(),"MATEKING_CHOICE");
+    }
+
+    @Test
+    public void beginOvertimeWorkTest() {
+        thread.beginOvertimeWork();
+        
+        assertEquals(sc.nextLine(),"OVERTIME_WORK");
+    }
+
+    @Test
+    public void beginUnregisterSubjectTest() {
+        thread.beginUnregisterSubject();
+        
+        assertEquals(sc.nextLine(),"UNREGISTER_SUBJECT");
+    }
+    
+    @Test
+    public void sendColorTest() 
     {
         
+        int color = 1;
+        thread.sendColor(color);
+        assertEquals(sc.nextLine(),"COLOR;" + color);
     }
-    */
+
+    @Test
+    public void showSubjectRegistrationWindowTest() 
+    {
+        thread.showSubjectRegistrationWindow();
+        
+        assertEquals(sc.nextLine(),"SHOW_SUBJECT_REGISTRATION_WINDOW");
+    }
+
+    @Test
+    public void activateTest() 
+    {
+        thread.activate();
+        
+        assertEquals(sc.nextLine(),"ACTIVATE");
+    }
+    
 }
