75a0577c5b78912d02c3e1a63a478d7ee2f7f080
Káré István <ifjkarei@gmail.com>
2019-04-26 18:50:14 +0200

    Update simian bat
    Command not found, let's see if this solves it

--- scripts/simianServer.bat
+++ scripts/simianServer.bat
@@ -1,3 +1,3 @@
 cd simian
-simian-2.5.10.exe -failOnDuplication + ../../server/src/main/java/com/gameofelte/**/*.java
+./simian-2.5.10.exe -failOnDuplication + ../../server/src/main/java/com/gameofelte/**/*.java
 cd ..
